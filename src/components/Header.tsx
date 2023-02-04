7783ddf5e72a4c7c1e916918134baea5f3f02ca9
DanielElport <36623369+DanielElport@users.noreply.github.com>
2023-02-05 00:47:06 +0100

    chore

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,32 +5,11 @@ import NavBar from './NavBar'
 const Header = () => {
     const [showHeader, setShowHeader] = React.useState(true)
 
-    React.useEffect(() => {
-        const handler = () => {
-            if (
-                document.body.scrollTop > 80 ||
-                document.documentElement.scrollTop > 80
-            ) {
-                setShowHeader(false)
-            } else {
-                setShowHeader(true)
-            }
-        }
-
-        window.addEventListener('scroll', handler)
-        return () => window.removeEventListener('scroll', handler)
-    })
-
     return (
         <>
             <div className="fixed-header position-fixed">
                 <div className={'container header-container p-0 '}>
-                    <div
-                        className={
-                            'jumbotron header-title mb-0 px-3 p-2  p-sm-3' +
-                            (showHeader ? '' : ' small')
-                        }
-                    >
+                    <div className="jumbotron header-title mb-0 px-3 p-2  p-sm-3">
                         <Link to="/">
                             <h2 className="page-title">
                                 <i
