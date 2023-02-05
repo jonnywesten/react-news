7a5ee0ca5395693176119d54fe8115ec7e81f29f
DanielElport <36623369+DanielElport@users.noreply.github.com>
2023-02-05 19:02:52 +0100

    chore

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ children }: { children: JSX.Element }) => {
             <div>
                 <Header />
                 <div className="container white-bg article-container px-sm-4">
-                    <div className="pt-2 pt-sm-3 pt-lg-4">{children}</div>
+                    <div className="pt-3 pt-lg-4">{children}</div>
                 </div>
             </div>
         </>
