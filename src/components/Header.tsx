f77c4bce7243b33dbdc99971618d5be1b4c40f89
DanielElport <36623369+DanielElport@users.noreply.github.com>
2023-02-05 02:33:13 +0100

    chore

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,24 +4,22 @@ import NavBar from './NavBar'
 
 const Header = () => {
     return (
-        <>
-            <div className="fixed-header position-fixed">
-                <div className={'container header-container p-0 '}>
-                    <div className="jumbotron header-title mb-0 px-3 p-2  p-sm-3">
-                        <Link to="/">
-                            <h2 className="page-title">
-                                <i
-                                    style={{ fontSize: '110%' }}
-                                    className="fa fa-globe mr-3"
-                                />
-                                Code Smart News
-                            </h2>
-                        </Link>
-                    </div>
-                    <NavBar />
+        <div className="fixed-header position-fixed">
+            <div className={'container header-container p-0 '}>
+                <div className="jumbotron header-title mb-0 px-3 p-2  p-sm-3">
+                    <Link to="/">
+                        <h2 className="page-title">
+                            <i
+                                style={{ fontSize: '110%' }}
+                                className="fa fa-globe mr-3"
+                            />
+                            Code Smart News
+                        </h2>
+                    </Link>
                 </div>
+                <NavBar />
             </div>
-        </>
+        </div>
     )
 }
 
