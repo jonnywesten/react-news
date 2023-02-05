fcf82dd4f280a39cefe6dfc79d7bcce815352494
DanielElport <36623369+DanielElport@users.noreply.github.com>
2023-02-05 19:03:21 +0100

    chore

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,14 +3,12 @@ import Header from './Header'
 
 const Layout = ({ children }: { children: JSX.Element }) => {
     return (
-        <>
-            <div>
-                <Header />
-                <div className="container white-bg article-container px-sm-4">
-                    <div className="pt-3 pt-lg-4">{children}</div>
-                </div>
+        <div>
+            <Header />
+            <div className="container white-bg article-container px-sm-4">
+                <div className="pt-3 pt-lg-4">{children}</div>
             </div>
-        </>
+        </div>
     )
 }
 
