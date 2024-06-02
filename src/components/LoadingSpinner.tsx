3f54676072193f29c385a7032363e8bbc9b96acd
DanielElport <dan.elport@gmail.com>
2024-06-03 01:12:53 +0200

    feat: add hover zoom effect

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LoadingSpinner = () => {
     return (
-        <div className="col text-center" style={{ padding: '64px 0' }}>
+        <div className="col text-center fade-in slow" style={{ padding: '64px 0' }}>
             <div className="loader" />
         </div>
     )
